7c5b52aca9bbf0bd5106a001e714db6aa8192fdc
Asantini <abe.santini@gmail.com>
2015-12-21 21:28:16 -0800

    finished halfIntervalMethod. Still need unit tests.

--- Sect_1_3_3_FunctionRoot/src/Sect_1_3_3_FunctionRoot.scala
+++ Sect_1_3_3_FunctionRoot/src/Sect_1_3_3_FunctionRoot.scala
@@ -1,22 +1,44 @@
 /**
  * Created by abes on 12/15/2015.
  */
-class Sect_1_3_3_FunctionRoot {
+object Sect_1_3_3_FunctionRoot {
 
   def main(args: Array[String]): Unit ={
-    search(((x: Double) => x), -1, 4)
+    println(search(squareMinusFour, -1.0, 4.0))
   }
 
-  def search(f: Function, negPoint: Double, posPoint: Double): Double {
-    val midpoint = average(List(f(negPoint), f(posPoint)))
+  def halfIntervalMethod(f: Double => Double, a: Double, b: Double) {
+    val aValue = f(a)
+    val bValue = f(b)
+    if(aValue < 0 && bValue > 0) search(f, a, b)
+    else if (bValue < 0 && aValue > 0) search(f, b, a)
+    else println("Values are not of opposite sign")
+
+  }
+
+  def search(f: Double => Double, negPoint: Double, posPoint: Double): Double = {
+    val midpoint = average(List(negPoint, posPoint))
+    println("Midpoint:" + midpoint)
+    if(isCloseEnogh(negPoint, posPoint)){
+      midpoint
+    } else{
+      val testValue = f(midpoint)
+      if (testValue > 0) search(f, negPoint, midpoint)
+      else if (testValue < 0) search(f, midpoint, posPoint)
+      else midpoint
+    }
+
   }
 
   def average(data: Iterable[Double]): Double = {
     data.sum / data.size
   }
 
-  def function1(x: Double): Double = {
-   x
+  def isCloseEnogh(a: Double, b: Double): Boolean = {
+    Math.abs(a - b) < .00000001
   }
 
+  def identity(x: Double): Double = x
+
+  def squareMinusFour(x:Double): Double = x*x - 4
 }
